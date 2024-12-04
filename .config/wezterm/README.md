4bd72986f8f2ee91b450e6d15cbaa8a003716bd5
tranlynhathao <haotran04022005@gmail.com>
2024-12-04 12:34:45 +0700

    add wezterm

---
+++ .config/wezterm/README.md
@@ -0,0 +1,7 @@
+# My Wezterm Configuration
+
+<hr>
+<p align="center"><img src="https://github.com/mgastonportillo/wezterm-config/assets/106234166/e0adaee3-0391-45e4-885b-6f5934ea3ea5"></p>
+<hr>
+My personal <a href="https://wezfurlong.org/wezterm/index.html">Wezterm</a> configuration
+
---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,58 @@
+---@type Wezterm
+local wezterm = require("wezterm")
+local mappings = require("wz-mappings") -- Đảm bảo `wz-mappings` không phụ thuộc vào hệ điều hành
+---@type Config
+local cf = wezterm.config_builder()
+
+local config = {
+	color_scheme = "Catppuccin Mocha",
+	font = wezterm.font_with_fallback({ "JetBrainsMono NF", "Hack Nerd Font" }),
+	font_size = 16,
+	line_height = 1.1,
+	window_background_opacity = 0.9,
+	window_padding = {
+		left = "20px",
+	},
+	keys = mappings,
+	disable_default_key_bindings = true,
+	default_domain = "local", -- Dùng "local" thay cho "WSL:Ubuntu"
+	window_decorations = "INTEGRATED_BUTTONS|RESIZE",
+	use_fancy_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = false,
+	launch_menu = {
+		{
+			label = "New Tab - ZSH",
+			args = { "/bin/zsh", "-l" },
+		},
+		{
+			label = "New Tab - Bash",
+			args = { "/bin/bash", "-l" },
+		},
+	},
+	min_scroll_bar_height = "0.5cell",
+	scrollback_lines = 50000,
+}
+
+for k, v in pairs(config) do
+	cf[k] = v
+end
+
+wezterm.on("window-focus-changed", function(window)
+	local overrides = window:get_config_overrides() or {}
+
+	if not overrides.colors then
+		overrides.colors = {}
+	end
+
+	if window:is_focused() then
+		overrides.colors.cursor_bg = nil
+	else
+		overrides.colors.cursor_border = "#222222"
+		overrides.colors.cursor_bg = "#222222"
+		overrides.colors.cursor_fg = "#222222"
+	end
+
+	window:set_config_overrides(overrides)
+end)
+
+return cf
