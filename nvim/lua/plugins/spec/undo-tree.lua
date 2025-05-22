7b09165f4178eabe480bf02b6129f1185aea81f0
tranlynhathao <haotran04022005@gmail.com>
2025-05-22 22:12:11 +0700

    Remove broken submodules nvim and catppuccin-tmux

--- nvim/lua/plugins/spec/undo-tree.lua
+++
@@ -1,8 +0,0 @@
----@type NvPluginSpec
-return {
-  "mbbill/undotree",
-  cmd = "UndotreeToggle",
-  init = function()
-    vim.keymap.set("n", "<leader>ut", "<cmd>UndotreeToggle<CR>", { desc = "UndoTree toggler" })
-  end,
-}
