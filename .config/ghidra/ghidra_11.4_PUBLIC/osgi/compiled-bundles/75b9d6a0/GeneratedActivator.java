9de49d9005a49b589cdc0e33d3b0727bf0a88fa9
tranlynhathao <haotran04022005@gmail.com>
2025-07-25 08:18:02 +0700

    weekly update

---
+++ .config/ghidra/ghidra_11.4_PUBLIC/osgi/compiled-bundles/75b9d6a0/GeneratedActivator.java
@@ -0,0 +1,12 @@
+import ghidra.app.plugin.core.osgi.GhidraBundleActivator;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+public class GeneratedActivator extends GhidraBundleActivator {
+  protected void start(BundleContext bc, Object api) {
+    // TODO: stuff to do on bundle start
+  }
+  protected void stop(BundleContext bc, Object api) {
+    // TODO: stuff to do on bundle stop
+  }
+
+}
